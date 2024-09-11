6d92a65a52a542d04c2bbddeee96129146222483
Harold Navarro <kaendor50@gmail.com>
2024-09-11 17:16:57 -0600

    Añadiendo datos de la pelicula

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Screen Match");
+        System.out.println("Movie: Matrix");
+
+        int releaseDate = 1999;
+        boolean planInclude = true;
+        double filmRating = 8.2;
+
+        double average = (8.2 + 6.0 + 9.0) / 3;
+
+        System.out.println(average);
     }
 }
